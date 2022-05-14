b6db2b5425cee119bccc52ec5066647304bb3346
Vidhya Rajendran <vidhya19@outlook.com>
2022-05-15 01:17:07 +0530

    Validations added for the input fields

--- PremiumCalculator/ClientApp/src/app/premium-calculator/premium-calculator.component.ts
+++ PremiumCalculator/ClientApp/src/app/premium-calculator/premium-calculator.component.ts
@@ -1,10 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
-import { getBaseUrl } from '../../main';
-
 @Component({
   selector: 'app-home',
-  templateUrl: './premium-calculator.html',
+  templateUrl: './premium-calculator.html'
 })
 export class PremiumCalculatorComponent {
   public showAge!: number;
@@ -14,7 +12,7 @@ export class PremiumCalculatorComponent {
   occupationFactor: any;
   occupations!: OccupationList[];
   baseServiceUrl: string = '';
-
+   
   constructor(public http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<OccupationList[]>(baseUrl + 'occupationlist').subscribe(result => {
       this.occupations = result;
@@ -23,7 +21,6 @@ export class PremiumCalculatorComponent {
   }
 
   calculateAge(event: any) {
-    console.log(event.target.value);
     if (event.target.value) {
       const convertAge = new Date(event.target.value);
       const timeDiff = Math.abs(Date.now() - convertAge.getTime());
@@ -45,10 +42,10 @@ export class PremiumCalculatorComponent {
         }
       }, error => console.error(error));
 
-  }
-  public calculatePremium() {
-    //Death Premium = (Death Cover amount * Occupation Rating Factor * Age) /1000 * 12
-    this.premiumAmount = (this.sumInsuredAmount * this.occupationFactor * this.showAge) / 1000 * 12;
+  }     
+  public calculatePremium() {  
+      //Death Premium = (Death Cover amount * Occupation Rating Factor * Age) /1000 * 12
+      this.premiumAmount = (this.sumInsuredAmount * this.occupationFactor * this.showAge) / 1000 * 12;
   }
 }
 interface OccupationList {
